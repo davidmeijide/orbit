4a7d0f10ae5072149affd1cd3cb440b206ade999
David <67502884+davidmeijide@users.noreply.github.com>
2024-09-09 07:48:49 +0000

    Added a script that updates the TLE file

--- src/get_gal_ephemeris.py
+++ src/get_gal_ephemeris.py
@@ -1,13 +1,9 @@
-import ephem
-import datetime
+import requests
+import os
+
 url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=galileo&FORMAT=tle"
 
-name = "ISS (ZARYA)";
-line1 = "1 37846U 11060A   24247.73900373 -.00000105  00000+0  00000+0 0  9992";
-line2 = "2 37846  57.1274 359.4123 0001042  74.3951 285.6259  1.70476006 80016";
-
-tle_rec = ephem.readtle(name, line1, line2);
-tle_rec.compute();
-
-print(tle_rec.sublong, tle_rec.sublat);
-print(tle_rec.x,tle_rec.y, tle_rec.z)
+response = requests.get(url)
+with open('../galileo_tle.txt', 'w') as file:
+    file.write(response.text)
+    print("File saved successfully.")
